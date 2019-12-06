74f71d05874230e98e29d9bab41dc7473020cd4c
luzhoumin <qc_b1d06392da9b43cb88407e47263f95fa@git.code.tencent.com>
2019-12-06 16:56:07 +0800

    文件上传添加大小限制，POST、GET方法添加log

--- src/main/java/com/luzhoumin/mblog/management/util/MHttpUtil.java
+++ src/main/java/com/luzhoumin/mblog/management/util/MHttpUtil.java
@@ -1,37 +1,69 @@
 package com.luzhoumin.mblog.management.util;
 
+import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.http.HttpUtil;
 import cn.hutool.json.JSONObject;
+import lombok.extern.slf4j.Slf4j;
 
+import java.util.Date;
 import java.util.Map;
 
+@Slf4j
 public class MHttpUtil {
 
 	public static JSONObject get(String requestKey, Map<String, Object> mapParams) {
-		//移除值为null的参数
-		Map<String, Object> newMap = MBlogCommonUtil.removeMapNullValue(mapParams);
+		Date startTime = new Date();
+		log.info("HTTP_GET_START : [Time:" + DateUtil.formatDateTime(startTime) + "]");
 
 		//获取请求地址
 		String uri = PropertiesUtil.getHttpRequestPath(requestKey);
+		log.info("HTTP_GET_URL : [" + uri + "]");
+
+		//参数设定
+		if (mapParams != null && mapParams.size() > 0) {
+			//移除值为null的参数
+			mapParams = MBlogCommonUtil.removeMapNullValue(mapParams);
+			log.info("HTTP_GET_PARAMS : [" + mapParams.toString() + "]");
+		} else {
+			log.info("HTTP_GET_PARAMS : 无参数");
+		}
 
 		//http请求
-		String post = HttpUtil.get(uri, newMap);
+		String post = HttpUtil.get(uri, mapParams);
+		log.info("HTTP_GET_RESULT : [" + post + "]");
 
+		Date endTime = new Date();
+		long diff = endTime.getTime() - startTime.getTime();
+		log.info("HTTP_GET_END : [Time:" + DateUtil.formatDateTime(endTime) + "][Total Time: " + diff + "ms]");
 		//返回结果
 		return new JSONObject(post);
 	}
 
 	public static JSONObject post(String requestKey, Map<String, Object> mapParams) {
-		//移除值为null的参数
-		Map<String, Object> newMap = MBlogCommonUtil.removeMapNullValue(mapParams);
+		Date startTime = new Date();
+		log.info("HTTP_POST_START : [Time:" + DateUtil.formatDateTime(startTime) + "]");
 
 		//获取请求地址
 		String uri = PropertiesUtil.getHttpRequestPath(requestKey);
+		log.info("HTTP_POST_URL : [" + uri + "]");
+
+		//参数设定
+		if (mapParams != null && mapParams.size() > 0) {
+			//移除值为null的参数
+			mapParams = MBlogCommonUtil.removeMapNullValue(mapParams);
+			log.info("HTTP_POST_PARAMS : [" + mapParams.toString() + "]");
+		} else {
+			log.info("HTTP_POST_PARAMS : 无参数");
+		}
 
 		//http请求
-		String post = HttpUtil.post(uri, newMap);
+		String post = HttpUtil.get(uri, mapParams);
+		log.info("HTTP_POST_RESULT : [" + post + "]");
 
+		Date endTime = new Date();
+		long diff = endTime.getTime() - startTime.getTime();
+		log.info("HTTP_POST_END : [Time:" + DateUtil.formatDateTime(endTime) + "][Total Time: " + diff + "ms]");
 		//返回结果
 		return new JSONObject(post);
 	}
